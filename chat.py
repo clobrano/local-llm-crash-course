8f22e3783f2080bc3cf2195e00a878df7a71a74d
Carlo Lobrano <c.lobrano@gmail.com>
2024-02-28 16:28:47 +0000

    Add basic UI with chainlit

--- chat.py
+++ chat.py
@@ -1,19 +1,30 @@
 from ctransformers import AutoModelForCausalLM
+import chainlit as cl
 
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
-)
+
+@cl.on_chat_start
+async def on_chat_start():
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+    )
+
+    await cl.Message("Model initialized. How can I help you?").send()
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content)
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+
+    await msg.update()
 
 
 def get_prompt(question: str) -> str:
     system = "You are an AI assistant that follows instruction extremely well. Give short answers."
     prompt = f"### System:\n{system}\n\n### User:\n{question}\n\n### Response:\n"
-    print(prompt, end="", flush=True)
     return prompt
-
-
-question = "Which is the capital of India?"
-
-for i in llm(get_prompt(question), stream=True):
-    print(i, end="", flush=True)
-print()
