193a014162a1018c5e0d73a307729b4c26159040
Carlo Lobrano <c.lobrano@gmail.com>
2024-02-28 15:51:30 +0000

    Format prompt in a function

--- chat.py
+++ chat.py
@@ -4,10 +4,16 @@ llm = AutoModelForCausalLM.from_pretrained(
     "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
 )
 
-system = "You are an AI assistant that follows instruction extremely well. Give short answers."
-instruction = "Which is the capital of India?"
 
-prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
-for i in llm(prompt, stream=True):
+def get_prompt(question: str) -> str:
+    system = "You are an AI assistant that follows instruction extremely well. Give short answers."
+    prompt = f"### System:\n{system}\n\n### User:\n{question}\n\n### Response:\n"
+    print(prompt, end="", flush=True)
+    return prompt
+
+
+question = "Which is the capital of India?"
+
+for i in llm(get_prompt(question), stream=True):
     print(i, end="", flush=True)
 print()
