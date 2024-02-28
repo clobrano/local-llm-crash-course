4d125590254b6bb107c9c0f246de3fb1b1eb9adb
Carlo Lobrano <c.lobrano@gmail.com>
2024-02-28 16:42:40 +0000

    Add minimal chat history support

--- chat.py
+++ chat.py
@@ -8,6 +8,7 @@ async def on_chat_start():
     llm = AutoModelForCausalLM.from_pretrained(
         "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
     )
+    cl.user_session.set("message_history", [])
 
     await cl.Message("Model initialized. How can I help you?").send()
 
@@ -17,14 +18,22 @@ async def main(message: cl.Message):
     msg = cl.Message(content="")
     await msg.send()
 
-    prompt = get_prompt(message.content)
+    message_history = cl.user_session.get("message_history")
+    prompt = get_prompt(message.content, message_history)
+    answer = ""
     for word in llm(prompt, stream=True):
         await msg.stream_token(word)
+        answer += word
+    message_history.append(answer)
 
     await msg.update()
 
 
-def get_prompt(question: str) -> str:
-    system = "You are an AI assistant that follows instruction extremely well. Give short answers."
-    prompt = f"### System:\n{system}\n\n### User:\n{question}\n\n### Response:\n"
+def get_prompt(question: str, history: list[str] = []) -> str:
+    system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
+    prompt = f"### System:\n{system}\n\n### User:\n"
+    if len(history) > 0:
+        prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
+    prompt += f"{question}\n\n### Response:\n"
+    print(f"Prompt created: {prompt}")
     return prompt
